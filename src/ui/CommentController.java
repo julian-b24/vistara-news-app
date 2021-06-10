73538f0a056b7da32dc9dfea02b6693b41da7fa0
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-06-10 15:40:13 -0500

    Comments adjustments for text

--- src/ui/CommentController.java
+++ src/ui/CommentController.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import model.Comment;
 import model.Vistara;
@@ -11,7 +12,7 @@ public class CommentController {
     private Text commentUser;
 
     @FXML
-    private Text commentContent;
+    private Label commentContent;
     
     public void setData(Comment comment, Vistara vis, VistaraGUI visG) {
     	commentUser.setText(comment.getAuthor());
